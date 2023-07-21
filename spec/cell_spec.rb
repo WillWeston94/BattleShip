6a1bf30ca5cceef820f6f6949eae530721f91bc8
Will Weston <Williamweston81@gmail.com>
2023-07-21 18:19:54 -0500

    Weston(4) Test ship = nil -pass Test empty? - pass Test place_ship @cruiser - pass Test ship = @cruiser - pass

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,10 +4,29 @@ require './lib/cell'
 RSpec.describe Cell do
   before(:each) do
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   it 'exists and has attributes' do
     expect(@cell).to be_an_instance_of(Cell)
     expect(@cell.coordinate).to eq("B4")
+  end
+
+  it 'shows ship' do
+    expect(@cell.ship).to eq(nil)
+  end
+
+  it 'checks for empty? cell' do
+    expect(@cell.empty?).to eq(true)
+  end
+
+  it 'places ship' do
+    expect(@cell.place_ship(@cruiser)).to eq(@cruiser)
+  end
+
+  it 'shows ship' do
+    @cell.place_ship(@cruiser)
+
+    expect(@cell.ship).to eq(@cruiser)
   end
 end
