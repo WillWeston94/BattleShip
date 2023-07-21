ce30f805476ff6f62f91d76cb0bf1001acc23bfe
Will Weston <Williamweston81@gmail.com>
2023-07-21 17:58:19 -0500

    Weston(4) Test cruiser attributes - pass Test sunk? method - pass Test hit method - pass Test hit 3 times and sunk = true - pass

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,6 +6,35 @@ RSpec.describe Ship do
   end
 
   it 'exists and has attributes' do
-    expect(@cruiser = Ship.new("Cruiser", 3)).to be_an_instance_of(Ship)
+    expect(@cruiser).to be_an_instance_of(Ship)
+    expect(@cruiser.length).to eq(3)
+    expect(@cruiser.health).to eq(3)
+  end
+
+  it 'is not sunk' do
+    expect(@cruiser.sunk?).to eq(false)
+  end
+
+  it 'it registers 1 hit' do
+    @cruiser.hit
+
+    expect(@cruiser.health).to eq(2)
+  end
+
+  it 'it registers 2 hits' do
+    @cruiser.hit
+    @cruiser.hit
+
+    expect(@cruiser.health).to eq(1)
+    expect(@cruiser.sunk?).to eq(false)
+  end
+
+  it 'it registers 3 hits and sunk? method' do
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+
+    expect(@cruiser.health).to eq(0)
+    expect(@cruiser.sunk?).to eq(true)
   end
 end
