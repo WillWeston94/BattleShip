56f323d86f7ceb605b32c1fbfb52a56ce8b7ac04
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-07-24 16:12:13 -0400

    Nurudeen(1) Test diagonal coordinates passed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -43,4 +43,11 @@ RSpec.describe Board do
     expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
     expect(@board.valid_placement?(@submarine, ["C1", "B1"])). to eq(false)
   end
+
+  it 'checks whether coordinates are diagonal' do
+    # binding.pry
+    expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to eq(false) 
+  end
+
 end
