f81a9943dc30799b8f5334d8c935c48d18a60fe4
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-07-24 15:35:53 -0400

    consecutive failed

--- lib/board.rb
+++ lib/board.rb
@@ -26,8 +26,17 @@ class Board
 
   def valid_placement?(ship, coordinates)
     return false unless ship.length == coordinates.count
-   
+  return false unless consecutive?(coordinates) 
   end 
+
+  def consecutive?(coordinates)
+    letter = coordinates.map { |coordinate| coordinate[0] }
+    number =  coordinates.map { |coordinate| coordinate[1..-1].to_i }
+
+    hor_consecutive = letter.uniq.length == 1 && number.each_cons(2).all? { |key, value| value == key + 1 || value == key - 1 }
+    vert_consecutive = number.uniq.length == 1 && letter.each_cons(2).all? { |key, value| value.ord == key.ord + 1 || value.ord == key.ord - 1 }
+    hor_consecutive || vert_consecutive
+  end
 end 
 
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,7 +32,13 @@ RSpec.describe Board do
 
   it 'checks length valid placement' do
     expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
-    expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
+  it 'checks whether coordinates are consecutive' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
+    expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq(false)
+  end
 end
